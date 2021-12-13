e27695c61f62b1ce004abfb689d47c2b0a4c8bb8
Pushp Raj <pushp.raj@gslab.com>
2021-12-13 12:12:53 +0530

    Added slice metrics generation

--- pkg/collector/slice.go
+++ pkg/collector/slice.go
@@ -4,7 +4,12 @@
 
 package collector
 
-import "time"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"math/rand"
+	"time"
+)
 
 type Slice struct {
 	SliceID      string   `json:"slice-id"`
@@ -15,4 +20,56 @@ type Slice struct {
 func (s *Slice) collect(period time.Duration, site string) {
 
 	// TODO: add in slice metrics generation
+	go func() {
+		minThroughput := 100000
+		maxThroughput := 1000000
+		for {
+			throughput := float64(rand.Intn(maxThroughput - minThroughput) + minThroughput)
+			sliceThroughputBytes.WithLabelValues(s.DisplayName, site).Set(throughput)
+			time.Sleep(period/10)
+		}
+	}()
+
+	go func() {
+		minPacket := 100
+		maxPacket := 800
+		for {
+			packets := float64(rand.Intn(maxPacket - minPacket) + minPacket)
+			sliceThroughputPackets.WithLabelValues(s.DisplayName, site).Set(packets)
+			time.Sleep(period/10)
+		}
+	}()
+
+	go func() {
+		minLatency := 10
+		maxLatency := 25
+		for {
+			latency := float64(rand.Intn(maxLatency - minLatency) + minLatency)
+			if latency != 17 {
+				sliceLatencyEndToEnd.WithLabelValues(s.DisplayName, site).Set(latency)
+				time.Sleep(period/10)
+			} else {
+				sliceLatencyEndToEnd.WithLabelValues(s.DisplayName, site).Set(100)
+				randomNum := rand.Intn(10)
+				time.Sleep(time.Duration(randomNum) * period)
+			}
+		}
+	}()
 }
+
+var (
+	sliceThroughputBytes = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "slice_throughput_bytes",
+		Help: "Slice Throughput",
+	}, []string{"slice", "site"})
+
+	sliceThroughputPackets = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "slice_throughput_packets",
+		Help: "Slice Throughput Packets",
+	}, []string{"slice", "site"})
+
+	sliceLatencyEndToEnd = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "slice_latency_end_to_end_milliseconds",
+		Help: "Slice Latency End-to-End",
+	}, []string{"slice", "site"})
+)

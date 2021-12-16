a2ee9c941e269eea6b04c650a681d565e544ca10
Sean Condon <sean@opennetworking.org>
2021-12-16 14:25:42 +0000

    handle CORS requests

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -42,9 +42,23 @@ func (mgr *Manager) Run() {
 }
 
 func (mgr *Manager) handleConfig(w http.ResponseWriter, req *http.Request) {
-	configJson, err := json.MarshalIndent(mgr.Config, " ", " ")
-	if err != nil {
-		fmt.Fprintf(w, "error marshalling model %s", err)
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	switch req.Method {
+	case "OPTIONS":
+		w.Header().Set("Vary", "Origin")
+		w.Header().Set("Vary", "Access-Control-Request-Method")
+		w.Header().Set("Vary", "Access-Control-Request-Headers")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.WriteHeader(http.StatusOK)
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		configJson, err := json.MarshalIndent(mgr.Config, " ", " ")
+		if err != nil {
+			fmt.Fprintf(w, "error marshalling model %s", err)
+		}
+		fmt.Fprintf(w, "%s", configJson)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
 	}
-	fmt.Fprintf(w, "%s", configJson)
 }

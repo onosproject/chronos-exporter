892a535538e0666a1550d7a316e83b04fce2b8cc
PUSHP RAJ <pushp.raj@gslab.com>
2021-12-23 13:18:16 +0000

    Added metrices per device per application

--- pkg/collector/site.go
+++ pkg/collector/site.go
@@ -11,9 +11,25 @@ import (
 func (s *Site) collect() {
 	// Start a go routine for any Site only metrics - if any
 
+	// getting the list of devices in each device group
+	var devices = make(map[string][]string)
+	for _, dg := range s.DeviceGroups {
+		devices[dg.DeviceGroupId] = *dg.Devices
+	}
+
 	// Then start the Slice's collector
 	for _, slice := range s.Slices {
 		slice.collect(time.Second * 10, s.SiteId)
+
+		if slice.Applications == nil || slice.DeviceGroups == nil {
+			return
+		}
+		for _, app := range *slice.Applications {
+			appId := app
+			for _, dg := range *slice.DeviceGroups {
+				slice.collectPerDevicePerApplication(time.Second * 10, s.SiteId, appId, devices[dg])
+			}
+		}
 	}
 
 	// and each Devices's collector

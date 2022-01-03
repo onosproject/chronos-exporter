0669a4d390a2f5ff6f7f9e65e671bea733e72531
Pushp Raj <67963061+praj527@users.noreply.github.com>
2022-01-03 11:19:30 +0530

    Added metrices per device per application (#16)

--- pkg/collector/slice.go
+++ pkg/collector/slice.go
@@ -51,6 +51,42 @@ func (s *Slice) collect(period time.Duration, site string) {
 	}()
 }
 
+func (s *Slice) collectPerDevicePerApplication(period time.Duration, site string, app string, devices []string) {
+	slice := s.SliceId
+	for _, device := range devices {
+		serialNumber := device
+		go func() {
+			minThroughput := 100000
+			maxThroughput := 1000000
+			for {
+				throughput := float64(rand.Intn(maxThroughput - minThroughput) + minThroughput)
+				perDevicePerApplicationThroughputBytes.WithLabelValues(site, slice, app, serialNumber).Set(throughput)
+				time.Sleep(period/10)
+			}
+		}()
+
+		go func() {
+			minPacket := 100
+			maxPacket := 800
+			for {
+				throughput := float64(rand.Intn(maxPacket - minPacket) + minPacket)
+				perDevicePerApplicationThroughputPacket.WithLabelValues(site, slice, app, serialNumber).Set(throughput)
+				time.Sleep(period/10)
+			}
+		}()
+
+		go func() {
+			minPacket := 1
+			maxPacket := 8
+			for {
+				throughput := float64(rand.Intn(maxPacket - minPacket) + minPacket)
+				perDevicePerApplicationLossPacket.WithLabelValues(site, slice, app, serialNumber).Set(throughput)
+				time.Sleep(period/10)
+			}
+		}()
+	}
+}
+
 var (
 	sliceThroughputBytes = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "slice_throughput_bytes",
@@ -66,4 +102,19 @@ var (
 		Name: "slice_latency_end_to_end_milliseconds",
 		Help: "Slice Latency End-to-End",
 	}, []string{"slice", "site"})
+
+	perDevicePerApplicationThroughputBytes = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "per_device_per_application_throughput_bytes",
+		Help: "Per Device Per Application Throughput Bytes",
+	}, []string{"site", "slice", "application", "serial_number"})
+
+	perDevicePerApplicationThroughputPacket = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "per_device_per_application_throughput_packets",
+		Help: "Per Device Per Application Throughput Packets",
+	}, []string{"site", "slice", "application", "serial_number"})
+
+	perDevicePerApplicationLossPacket = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "per_device_per_application_loss_packets",
+		Help: "Per Device Per Application Loss Packet",
+	}, []string{"site", "slice", "application", "serial_number"})
 )

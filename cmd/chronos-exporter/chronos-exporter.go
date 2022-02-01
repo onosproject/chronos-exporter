64fb441fd7b6f5760e2520daf36f94f6f1c284e3
Pushp Raj <pushp.raj@gslab.com>
2022-02-01 20:13:38 +0530

    Implemented the load alert from helm chart

--- cmd/chronos-exporter/chronos-exporter.go
+++ cmd/chronos-exporter/chronos-exporter.go
@@ -14,6 +14,7 @@ import (
 var log = logging.GetLogger("main")
 
 func main() {
+	alertPath := flag.String("alert", "", "path to alert file")
 	configPath := flag.String("config", "", "path to configuration file")
 	imagePath := flag.String("imagePath", "/opt/", "path to the images")
 	sitePlanPath := flag.String("sitePlanPath", "/opt/", "path to the site plans")
@@ -24,13 +25,23 @@ func main() {
 		log.Fatal("No config specified. Cannot start")
 	}
 
+	if alertPath == nil || *alertPath == "" {
+		log.Fatal("No alert specified. Cannot start")
+	}
+
 	log.Infof("Starting chronos-exporter with config: %s", *configPath)
 	configJSON, err := ioutil.ReadFile(*configPath)
 	if err != nil {
 		log.Fatalf("Cannot start without a valid config %v", err)
 	}
 
-	mgr := manager.NewManager(configJSON, *imagePath, *sitePlanPath,
+	log.Infof("Starting chronos-exporter with alert: %s", *alertPath)
+	alertJSON, err := ioutil.ReadFile(*alertPath)
+	if err != nil {
+		log.Fatalf("Cannot start without a alert file %v", err)
+	}
+
+	mgr := manager.NewManager(configJSON, alertJSON, *imagePath, *sitePlanPath,
 		[]string{"http://localhost:4200"})
 	mgr.Run(2112)
 

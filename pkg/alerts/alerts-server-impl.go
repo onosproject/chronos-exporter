d86d4d68fb8cf675a3a8fd6d39c9aa633f9b2a58
Pushp Raj <67963061+praj527@users.noreply.github.com>
2022-02-01 20:50:31 +0530

    Aether-3045: Load Alert from helm chart (#35)
    * Implemented the load alert from helm chart
    
    * Fixed linting error

--- pkg/alerts/alerts-server-impl.go
+++ pkg/alerts/alerts-server-impl.go
@@ -6,23 +6,58 @@ package alerts
 
 import (
 	"github.com/labstack/echo/v4"
+	"gopkg.in/yaml.v2"
 	"net/http"
+	"time"
 )
 
 // ServerImpl -
 type ServerImpl struct {
+	alert *AetherAlerts
 }
 
-func NewServer() ServerInterface {
-	return new(ServerImpl)
+type response struct {
+	Message string `json:"message"`
+	AlertId string `json:"alert-id"`
+}
+
+func NewServer(alert *AetherAlerts) ServerInterface {
+	return &ServerImpl{
+		alert: alert,
+	}
+}
+
+func LoadModel(modelData []byte) (*AetherAlerts, error) {
+
+	model := new(AetherAlerts)
+
+	err := yaml.UnmarshalStrict(modelData, model)
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
 }
 
 // GetAetherAlerts (GET /alerts)
 func (s *ServerImpl) GetAetherAlerts(ctx echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented)
+	return ctx.JSON(http.StatusOK, s.alert)
 }
 
 // ClearAlert (POST /alerts/{alert-id}/clear)
 func (s *ServerImpl) ClearAlert(ctx echo.Context, alertId string) error {
-	return echo.NewHTTPError(http.StatusNotImplemented)
+
+	for index, al := range *s.alert {
+		if al.AlertId == alertId {
+			timeNow := time.Now()
+			s1 := *s.alert
+			s1[index].ClearedAt = &timeNow
+			resp := &response{
+				Message: "Alert cleared",
+				AlertId: alertId,
+			}
+			return ctx.JSON(http.StatusOK, &resp)
+		}
+	}
+
+	return echo.NewHTTPError(http.StatusNotFound)
 }
